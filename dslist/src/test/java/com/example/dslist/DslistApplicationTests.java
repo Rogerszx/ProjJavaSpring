86cfbf92ee282975a626a99bce0869dcaab9ed86
Rogerszx <104950900+Rogerszx@users.noreply.github.com>
2025-09-15 14:09:10 -0300

    Add files via upload
    "Project Created"

---
+++ dslist/src/test/java/com/example/dslist/DslistApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
