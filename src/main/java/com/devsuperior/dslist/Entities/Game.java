02f9fa78adbd093aaf89b99afde2ad45662e9d0c
Rogerszx <104950900+Rogerszx@users.noreply.github.com>
2025-09-17 11:14:12 -0300

    properties, game, ORM

--- src/main/java/com/devsuperior/dslist/Entities/Game.java
+++ src/main/java/com/devsuperior/dslist/Entities/Game.java
@@ -1,8 +1,13 @@
 package com.devsuperior.dslist.Entities;
 
-import jakarta.persistence.*;
-
 import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_game")
@@ -11,22 +16,34 @@ public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
 
     @Column(name = "game_year")
     private Integer year;
+
+    private Double score;
+
     private String genre;
+
+    @Column(name = "platforms")
     private String platform;
+
     private String imgUrl;
+
+    @Column(columnDefinition="TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition="TEXT")
     private String longDescription;
 
     protected Game() {}
 
-    public Game(String title, Long id, Integer year, String platform, String genre, String imgUrl, String shortDescription, String longDescription) {
+    public Game(String title, Long id, Integer year, Double score, String platform, String genre, String imgUrl, String shortDescription, String longDescription) {
         this.title = title;
         this.id = id;
         this.year = year;
+        this.score = score;
         this.platform = platform;
         this.genre = genre;
         this.imgUrl = imgUrl;
@@ -56,6 +73,14 @@ public class Game {
 
     public void setYear(Integer year) {
         this.year = year;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getGenre() {
